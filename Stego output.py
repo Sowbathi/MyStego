98bcf32b27198f6ad9e417fe61155d93fc0a32d2
Sowbathi <sowbathi@gmail.com>
2025-02-19 22:01:02 +0100

    Delete Stego output.py

--- Stego output.py
+++
@@ -1,8 +0,0 @@
-Python 3.13.2 (tags/v3.13.2:4f8bb39, Feb  4 2025, 15:23:48) [MSC v.1942 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> 
-= RESTART: C:\Users\Sowbathi.DESKTOP-RF7E79C\Desktop\Stego project\Stego code.py
-Enter secret message:Hello my dear friend!
-Enter a passcode:12345
-Enter passcode for Decryption12345
-Decryption message: Hello my dear friend!
